1f75e2e2879916717b1b8357f1c45cae85c01562
shamshad <shamshadalam10313@gmail.com>
2025-12-05 21:50:43 +0530

    Fix production route for Render

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.use((req, res) => {
+   app.get("/*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
