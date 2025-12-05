3fb85dfb514c29a4988dc6f2698872a3ecc7af4d
shamshad <shamshadalam10313@gmail.com>
2025-12-05 21:44:33 +0530

    complet

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
