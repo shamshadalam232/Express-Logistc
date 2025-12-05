577d20b2665c052961a59656310de2bc2e889914
shamshad <shamshadalam10313@gmail.com>
2025-12-05 22:52:12 +0530

    Fix axios baseURL and add Vite env files

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
