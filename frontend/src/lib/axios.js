17741c38b15aaea65b45bccc47b5c64a4a694918
shamshad <shamshadalam10313@gmail.com>
2025-12-05 22:27:24 +0530

    Fix API baseURL for Vite production on Render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:  import.meta.env.MODE === "devlopment" ? "http://localhost:5000/api" : "/api",
+  baseURL:  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
 });
