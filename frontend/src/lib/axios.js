f8e3e4d0571223572cc53572e43934073bb91cf7
shamshad <shamshadalam10313@gmail.com>
2025-12-06 15:10:21 +0530

    all set

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "/api",
   withCredentials: true,
 });
 
