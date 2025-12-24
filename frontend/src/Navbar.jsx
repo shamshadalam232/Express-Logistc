ee9b40378cb2aac0cbe2085b921f76cf5359c554
shamshad <shamshadalam10313@gmail.com>
2025-12-24 19:39:20 +0530

    last time

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
           {isAdminPage && (
             <li>
               <a 
-                href="https://mail.zoho.in/zm/#mail/folder/inbox" 
+                href="https://webmail.migadu.com/#/mailbox/INBOX" 
                 target="_blank"
                 className="hover:text-blue-300"
               >
@@ -81,7 +81,7 @@ export default function Navbar() {
             {isAdminPage && (
               <li>
                 <a 
-                  href="https://mail.zoho.in/zm/#mail/folder/inbox"
+                  href="https://webmail.migadu.com/#/mailbox/INBOX"
                   target="_blank"
                   onClick={() => setOpen(false)}
                 >
